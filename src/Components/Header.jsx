a39c6fd17820486b7e4164e544c76def403a6069
[anudeep avula] <[anudeepavula009@gmail.com]>
2024-06-12 17:37:27 +0530

    Revert "update"
    This reverts commit 4259c98a068d87151b1f0438ee97dd0b05f67c4f.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,6 @@ import Trending from './Trending';
 import { Signup } from '../Exports';
 import {Signin} from '../Exports';
 import { Toaster } from 'react-hot-toast';
-import Home from './Home'
 
 const menuItems = [
   { name: 'Market' },
